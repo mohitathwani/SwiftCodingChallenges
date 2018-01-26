e8f86f454bca5780863c8a41a857c8d26f16c057
Mohit Athwani <mohit.athwani@gmail.com>
2018-01-26 12:33:12 -0800

    Solution for Same Tree

---
+++ sameTree/SameTree.playground/Contents.swift
@@ -0,0 +1,59 @@
+//https://leetcode.com/problems/same-tree/description/
+//Given two binary trees, write a function to check if they are the same or not.
+//
+//Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+//
+//
+//Example 1:
+//
+//Input:     1         1
+/// \       / \
+//2   3     2   3
+//
+//[1,2,3],   [1,2,3]
+//
+//Output: true
+//Example 2:
+//
+//Input:     1         1
+///           \
+//2             2
+//
+//[1,2],     [1,null,2]
+//
+//Output: false
+//Example 3:
+//
+//Input:     1         1
+/// \       / \
+//2   1     1   2
+//
+//[1,2,1],   [1,1,2]
+//
+//Output: false
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    if p == nil && q == nil {
+        return true
+    }
+    if p?.val == q?.val {
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+    
+    return false
+}
+
