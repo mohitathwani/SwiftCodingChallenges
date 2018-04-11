b1adbd743310b46cd55eb8b9d66bb38890f2e43a
Mohit Athwani <mohit.athwani@gmail.com>
2018-04-11 11:01:16 -0700

    Trying the expand around center approach(not working yet)

--- longestPalindromicSubstring/LongestPalindromicSubstring.playground/Contents.swift
+++ longestPalindromicSubstring/LongestPalindromicSubstring.playground/Contents.swift
@@ -57,3 +57,45 @@ func longestPalindromeSubstring(_ s:String) -> String {
     let endIndex = s.index(beginningIndex, offsetBy: maxLen)
     return String(s[beginningIndex..<endIndex])
 }
+
+func longestPalindrome(_ s:String) -> String {
+    if(s.count == 1 || s.isEmpty) {
+        return s
+    }
+    
+    let charArray = Array(s)
+    var startIndex = 0
+    var maxLen = 0
+    
+    for i in 1..<s.count {
+        let (start, length) = expandAround(i, j: i, charArray: charArray)
+        print(start, length)
+        if length > maxLen {
+            startIndex = start
+            maxLen = length
+        }
+    }
+    
+    let beginningIndex = s.index(s.startIndex, offsetBy: (startIndex < 0) ? 0 : startIndex)
+    let endIndex = s.index(beginningIndex, offsetBy: maxLen)
+    return String(s[beginningIndex..<endIndex])
+}
+
+func expandAround(_ i: Int, j: Int, charArray:[Character]) -> (startIndex:Int, length:Int) {
+    var leftIndex = i - 1
+    var rightIndex = j + 1
+    var curLen = 1
+    
+    while leftIndex >= 0 && rightIndex < charArray.count {
+        print(leftIndex, rightIndex, curLen)
+        if(charArray[leftIndex] == charArray[rightIndex]) {
+            leftIndex -= 1
+            rightIndex += 1
+            curLen = rightIndex - leftIndex + 1
+        }
+    }
+    
+    return (leftIndex, curLen)
+}
+
+longestPalindrome("aaa")
