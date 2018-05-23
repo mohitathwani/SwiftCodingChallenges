f3043adfecc94c37b89681106eb96350205d1e57
Mohit Athwani <mohit@athwani.net>
2018-05-22 20:26:35 -0700

    StringToInt is now complete

--- stringToInteger/StringToInteger.playground/Contents.swift
+++ stringToInteger/StringToInteger.playground/Contents.swift
@@ -41,46 +41,80 @@
 //Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
 //Thefore INT_MIN (−231) is returned.
 
+enum States {
+    case start, space, minus, plus, digit, alphaChar, end
+}
+
+func isNextStateValid(curState: States, nextState: States) -> Bool {
+    switch curState {
+    case .start:
+        return nextState == .space || nextState == .minus || nextState == .digit || nextState == .plus
+        
+    case .space:
+        return nextState == .space || nextState == .minus || nextState == .digit || nextState == .plus
+        
+    case .minus:
+        return nextState == .digit
+        
+    case .plus:
+        return nextState == .digit
+        
+    case .digit:
+        return nextState == .digit
+    default:
+        return false
+    }
+}
+
 func myAtoi(_ str: String) -> Int {
     let charArray = Array(str)
+    var curState: States = .start
     
     var result = 0
-    var digitSeen = false
     var multiplier = 1
     
     for char in charArray {
-        if char == "+"{
-            if digitSeen {
-               break
-            }
-            multiplier = 1
+        if char == " " && isNextStateValid(curState: curState, nextState: .space){
+            curState = .space
             continue
         }
         
-        if char == "-" {
-            if digitSeen {
-                break
-            }
+        if char == "+" && isNextStateValid(curState: curState, nextState: .plus) {
+            curState = .plus
+            continue
+        }
+        
+        if char == "-" && isNextStateValid(curState: curState, nextState: .minus) {
+            curState = .minus
             multiplier = -1
             continue
         }
-        if char == " "{
-            if digitSeen {
+        
+        if let digit = Int(String(char)), isNextStateValid(curState: curState, nextState: .digit) {
+            curState = .digit
+            result = result * 10 + digit
+            
+            if(result > Int32.max) {
                 break
             }
-            continue
-        }
-        
-        if let digit = Int(String(char)) {
-            digitSeen = true
-            result = result * 10 + digit
         } else {
             break
         }
     }
     
-    return result * multiplier
+    result = result * multiplier
+    
+    if result < Int32.min {
+        return Int(Int32.min)
+    }
+    
+    if result > Int32.max {
+        return Int(Int32.max)
+    }
+    
+    return result
 }
+
 
 myAtoi("   ") == 0
 myAtoi("  12") == 12
@@ -91,3 +125,10 @@ myAtoi("123asd") == 123
 myAtoi("+123") == 123
 myAtoi("-123") == -123
 myAtoi("   + 123") == 0
+myAtoi("  - 123") == 0
+myAtoi("+-123") == 0
+myAtoi("42") == 42
+myAtoi("   -42") == -42
+print(Int.max)
+myAtoi("20000000000000000000")
+
