1cf1ed03a0df92fe0b277469a8088a18dbbbfd9f
Mohit Athwani <mohit.athwani@gmail.com>
2018-06-20 14:03:43 -0700

    Regex with DP

--- regularExpressionMatching/RegularExpressionMatching.playground/Contents.swift
+++ regularExpressionMatching/RegularExpressionMatching.playground/Contents.swift
@@ -70,19 +70,30 @@ func isMatch(_ string: String, _ pattern: String) -> Bool {
   var T = [[Bool]](repeating: [Bool](repeating: false, count: pattern.count + 1), count: string.count + 1)
   
   T[0][0] = true
-  
-  if string.count < 1 || pattern.count < 1 {
-    return false // probably needs thinking...
-  }
-  
-  for i in 1...string.count {
-    for j in 1...pattern.count {
-      if string[i - 1] == pattern[j - 1] || pattern[j - 1] == "." {
-        T[i][j] = T[i - 1][j - 1];
+
+  //For patterns like a*, a*b*c* etc...
+  if pattern.count > 0 {
+    for i in 1...pattern.count {
+      if pattern[i - 1] == "*" {
+        T[0][i] = T[0][i - 2]
       }
     }
   }
   
+  if string.count > 0 {
+    for i in 1...string.count {
+      if pattern.count > 0 {
+        for j in 1...pattern.count {
+          if string[i - 1] == pattern[j - 1] || pattern[j - 1] == "." {
+            T[i][j] = T[i - 1][j - 1];
+          }
+          if pattern[j - 1] == "*" {
+            T[i][j] = T[i][j - 2] || (T[i - 1][j] && (string[i - 1] == pattern[j - 2] || pattern[j - 2] == "."))
+          }
+        }
+      }
+    }
+  }
   
   return T[string.count][pattern.count]
   
@@ -94,9 +105,11 @@ isMatch("a", "a.") == false
 isMatch("aaa", "a.") == false
 isMatch("a", "aa.") == false
 isMatch("abc", "a.c") == true
-//isMatch("a", "a*") == true
-//isMatch("a", ".*") == true
-//isMatch("aaabbcd", "a*b*..") == true
-//isMatch("cd", "b*..") == true
-//isMatch("aaacd", "a*b*..") == true
-//isMatch("aaaaaaaaaaaaa", ".*") == true
+isMatch("a", "a*") == true
+isMatch("a", ".*") == true
+isMatch("aaabbcd", "a*b*..") == true
+isMatch("cd", "b*..") == true
+isMatch("aaacd", "a*b*..") == true
+isMatch("aaaaaaaaaaaaa", ".*") == true
+isMatch("ac", "a.*c") == false
+isMatch("", ".*")
