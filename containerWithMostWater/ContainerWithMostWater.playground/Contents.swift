e149213cd22a67e744e1cb5e03f553e366ad84f1
Mohit Athwani <mohit.athwani@gmail.com>
2018-06-21 10:51:21 -0700

    Container with most water complete

--- containerWithMostWater/ContainerWithMostWater.playground/Contents.swift
+++ containerWithMostWater/ContainerWithMostWater.playground/Contents.swift
@@ -4,5 +4,29 @@
 //Note: You may not slant the container and n is at least 2.
 
 func maxArea(_ height: [Int]) -> Int {
+  var start = 0
+  var end = height.count - 1
   
+  var maxArea = 0
+  
+  for _ in 0..<height.count {
+    if start == end {
+      break
+    }
+    
+    let left = height[start]
+    let right = height[end]
+    
+    maxArea = max(min(left, right) * (end - start), maxArea)
+    if left < right {
+      start += 1
+    } else {
+      end -= 1
+    }
+  }
+  
+  return maxArea
 }
+
+maxArea([4,3,7,8,2,6,9,5])
+maxArea([0,1])
