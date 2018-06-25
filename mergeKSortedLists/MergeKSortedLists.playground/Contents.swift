6d2a31ddd1734e6d2987663c7a2c10ed8cb1ffc5
Mohit Athwani <mohit@athwani.net>
2018-06-24 19:37:53 -0700

    Merge K Sorted lists done

--- mergeKSortedLists/MergeKSortedLists.playground/Contents.swift
+++ mergeKSortedLists/MergeKSortedLists.playground/Contents.swift
@@ -20,13 +20,44 @@ public class ListNode {
   }
 }
 
-//func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
-
-//}
+func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+  guard lists.count > 0 else {
+    return nil
+  }
+  
+  guard lists.count >= 2 else {
+    return lists.first!
+  }
+  
+  var tempLists = lists
+  
+  while tempLists.count > 1 {
+    guard let l1 = tempLists[0] else {
+      tempLists.removeFirst()
+      continue
+    }
+    
+    tempLists.removeFirst()
+    guard let l2 = tempLists[0] else {
+      tempLists.removeFirst()
+      tempLists.append(l1)
+      continue
+    }
+    
+    tempLists.removeFirst()
+    tempLists.append(merge(l1: l1, l2: l2))
+  }
+  
+  return tempLists.first!
+}
 
 func merge(l1: ListNode, l2: ListNode) -> ListNode? {
   var list1: ListNode? = l1
   var list2: ListNode? = l2
+  
+  if l1 === l2 {
+    return l1
+  }
   
   let result = ListNode(0)
   var tail: ListNode? = result
@@ -63,16 +94,22 @@ func merge(l1: ListNode, l2: ListNode) -> ListNode? {
   
 }
 
-let l1 = ListNode(1)
-l1.next = ListNode(3)
-l1.next?.next = ListNode(5)
+let l1 = ListNode(-2)
+l1.next = ListNode(-1)
+l1.next?.next = ListNode(-1)
 
 let l2 = ListNode(2)
 l2.next = ListNode(4)
 l2.next?.next = ListNode(6)
 l2.next?.next?.next = ListNode(8)
 
-var result = merge(l1: l1, l2: l2)
+let l3 = ListNode(0)
+l3.next = ListNode(3)
+l3.next?.next = ListNode(7)
+
+
+var result = mergeKLists([l1, nil])
+//var result = merge(l1: l1, l2: l2)
 while result != nil {
   print(result?.val)
   result = result?.next
