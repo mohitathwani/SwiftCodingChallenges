212b780826ad251f48df70f87a253f514cd2abbf
Mohit Athwani <mohit@athwani.net>
2018-07-05 16:45:48 -0700

    Word search II almost done

--- wordSearchII/WordSearchII.playground/Contents.swift
+++ wordSearchII/WordSearchII.playground/Contents.swift
@@ -18,8 +18,97 @@
 //Note:
 //You may assume that all inputs are consist of lowercase letters a-z.
 
-func dfs(i: Int, j: Int, index: Int, chars: [Character], board:[[Character]], map:inout [[Bool]]) -> Bool {
-  
+//func dfs(i: Int, j: Int, index: Int, chars: [Character], board:[[Character]], map:inout [[Bool]]) -> Bool {
+//
+//  let topRow = i - 1
+//  let bottomRow = i + 1
+//  let leftColumn = j - 1
+//  let rightColumn = j + 1
+//
+//  let numRows = board.count
+//  let numCols = board[0].count
+//
+//  let isLastChar = index == chars.count - 1
+//
+//  if isLastChar {
+//    return board[i][j] == chars[index]
+//  }
+//
+//  if topRow >= 0 && !map[topRow][j] && !isLastChar && board[topRow][j] == chars[index + 1] {
+//    map[topRow][j] = true
+//    let result = dfs(i: topRow, j: j, index: index + 1, chars: chars, board: board, map: &map)
+//    if result == true {
+//      return true
+//    }
+//    map[topRow][j] = false
+//  }
+//
+//  if bottomRow < numRows && !map[bottomRow][j] && !isLastChar && board[bottomRow][j] == chars[index + 1] {
+//    map[bottomRow][j] = true
+//    let result = dfs(i: bottomRow, j: j, index: index + 1, chars: chars, board: board, map: &map)
+//    if result == true {
+//      return true
+//    }
+//    map[bottomRow][j] = false
+//  }
+//
+//  if leftColumn >= 0 && !map[i][leftColumn] && !isLastChar && board[i][leftColumn] == chars[index + 1] {
+//    map[i][leftColumn] = true
+//    let result = dfs(i: i, j: leftColumn, index: index + 1, chars: chars, board: board, map: &map)
+//    if result == true {
+//      return true
+//    }
+//    map[i][leftColumn] = false
+//  }
+//
+//  if rightColumn < numCols && !map[i][rightColumn] && !isLastChar && board[i][rightColumn] == chars[index + 1] {
+//    map[i][rightColumn] = true
+//    let result = dfs(i: i, j: rightColumn, index: index + 1, chars: chars, board: board, map: &map)
+//    if result == true {
+//      return true
+//    }
+//    map[i][rightColumn] = false
+//  }
+//
+//  return false
+//}
+//
+//func exist(_ board: [[Character]], _ word: String) -> Bool {
+//
+//  let numRows = board.count
+//  let numCols = numRows > 0 ? board[0].count : 0
+//  let chars = Array(word)
+//
+//  if numRows == 0 || numCols == 0 || word.isEmpty{
+//    return false
+//  }
+//
+//  if word.count >  numRows * numCols {
+//    return false
+//  }
+//
+//  var map = board.map { (row) -> [Bool] in
+//    row.map({ (c) -> Bool in
+//      return false
+//    })
+//  }
+//
+//  for i in stride(from: 0, to: numRows, by: 1) {
+//    for j in stride(from: 0, to: numCols, by: 1) {
+//      if board[i][j] == chars[0] {
+//        map[i][j] = true
+//        let result = dfs(i: i, j: j, index: 0, chars: chars, board: board, map: &map)
+//        if result == true {
+//          return true
+//        }
+//        map[i][j] = false
+//      }
+//    }
+//  }
+//  return false
+//}
+
+func dfs(i: Int, j: Int, node: TrieNode, map:inout [[Bool]], board:[[Character]]) -> String? {
   let topRow = i - 1
   let bottomRow = i + 1
   let leftColumn = j - 1
@@ -27,92 +116,83 @@ func dfs(i: Int, j: Int, index: Int, chars: [Character], board:[[Character]], ma
   
   let numRows = board.count
   let numCols = board[0].count
-  
-  let isLastChar = index == chars.count - 1
-  
-  if isLastChar {
-    return board[i][j] == chars[index]
+
+  if let word = node.word {
+//    print(word)
+    return word
   }
-  
-  if topRow >= 0 && !map[topRow][j] && !isLastChar && board[topRow][j] == chars[index + 1] {
+  print(i,j)
+  if topRow >= 0 && !map[topRow][j] && node[board[topRow][j]] != nil {
     map[topRow][j] = true
-    let result = dfs(i: topRow, j: j, index: index + 1, chars: chars, board: board, map: &map)
-    if result == true {
-      return true
+    let string = dfs(i: topRow, j: j, node: node[board[topRow][j]]!, map: &map, board: board)
+    if let str = string {
+      return str
     }
     map[topRow][j] = false
   }
   
-  if bottomRow < numRows && !map[bottomRow][j] && !isLastChar && board[bottomRow][j] == chars[index + 1] {
+  if bottomRow < numRows && !map[bottomRow][j] && node[board[bottomRow][j]] != nil {
     map[bottomRow][j] = true
-    let result = dfs(i: bottomRow, j: j, index: index + 1, chars: chars, board: board, map: &map)
-    if result == true {
-      return true
+    let string = dfs(i: bottomRow, j: j, node: node[board[bottomRow][j]]!, map: &map, board: board)
+    if let str = string {
+      return str
     }
     map[bottomRow][j] = false
   }
   
-  if leftColumn >= 0 && !map[i][leftColumn] && !isLastChar && board[i][leftColumn] == chars[index + 1] {
+  if leftColumn >= 0 && !map[i][leftColumn] && node[board[i][leftColumn]] != nil {
     map[i][leftColumn] = true
-    let result = dfs(i: i, j: leftColumn, index: index + 1, chars: chars, board: board, map: &map)
-    if result == true {
-      return true
+    print("Checking left", node)
+    let string = dfs(i: i, j: leftColumn, node: node[board[i][leftColumn]]!, map: &map, board: board)
+    if let str = string {
+      return str
     }
     map[i][leftColumn] = false
   }
   
-  if rightColumn < numCols && !map[i][rightColumn] && !isLastChar && board[i][rightColumn] == chars[index + 1] {
+  if rightColumn < numCols && !map[i][rightColumn] && node[board[i][rightColumn]] != nil {
     map[i][rightColumn] = true
-    let result = dfs(i: i, j: rightColumn, index: index + 1, chars: chars, board: board, map: &map)
-    if result == true {
-      return true
+    let string = dfs(i: i, j: rightColumn, node: node[board[i][rightColumn]]!, map: &map, board: board)
+    if let str = string {
+      return str
     }
     map[i][rightColumn] = false
   }
   
-  return false
+  return nil
 }
 
-func exist(_ board: [[Character]], _ word: String) -> Bool {
+func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
   
-  let numRows = board.count
-  let numCols = numRows > 0 ? board[0].count : 0
-  let chars = Array(word)
-  
-  if numRows == 0 || numCols == 0 || word.isEmpty{
-    return false
+  if board.isEmpty || words.isEmpty {
+    return [String]()
   }
   
-  if word.count >  numRows * numCols {
-    return false
-  }
-  
+  var result = [String]()
+  let root = generateTrie(words: words)
+//  print(root)
   var map = board.map { (row) -> [Bool] in
     row.map({ (c) -> Bool in
       return false
     })
   }
   
+  let numRows = board.count
+  let numCols = board[0].count
+  
   for i in stride(from: 0, to: numRows, by: 1) {
     for j in stride(from: 0, to: numCols, by: 1) {
-      if board[i][j] == chars[0] {
-        map[i][j] = true
-        let result = dfs(i: i, j: j, index: 0, chars: chars, board: board, map: &map)
-        if result == true {
-          return true
+      if root[board[i][j]] != nil {
+//        print(board[i][j])
+        let string = dfs(i: i, j: j, node: root[board[i][j]]!, map: &map, board: board)
+        if let str = string {
+          result.append(str)
         }
-        map[i][j] = false
       }
     }
   }
-  return false
-}
-
-func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
   
-  var root = generateTrie(words: words)
-  
-  return [String]()
+  return result
 }
 
 func generateTrie(words: [String]) -> TrieNode {
@@ -120,18 +200,25 @@ func generateTrie(words: [String]) -> TrieNode {
   
   for word in words {
     var temp = root
-    for c in word {
+    for (i,c) in word.enumerated() {
       if temp[c] == nil {
         temp[c] = TrieNode()
       }
-      
+      if i == word.count - 1 {
+        temp[c]?.word = word
+      }
+      temp = temp[c]!
     }
   }
   
   return root
 }
 
-class TrieNode {
+class TrieNode: CustomStringConvertible {
+  var description: String {
+    return "\(children.description) -> \(word)"
+  }
+  
   var children = [Character:TrieNode]()
   var word: String?
   
@@ -145,6 +232,8 @@ class TrieNode {
     
   }
 }
-exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")
-exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE")
-exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB")
+
+findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["oath","pea","eat","rain"])
+//exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")
+//exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE")
+//exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB")
