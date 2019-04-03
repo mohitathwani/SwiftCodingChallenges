485ecb5e1756b7fb5ca9161147098fe5c87571fc
Mohit Athwani <mohit@athwani.net>
2019-04-02 20:35:42 -0700

    Coins with DP done

--- coins/Coins.playground/Contents.swift
+++ coins/Coins.playground/Contents.swift
@@ -33,3 +33,34 @@ func recursiveNumWays(denominations:[Int], num: Int) -> Int {
 }
 
 recursiveNumWays(denominations: [25,10,5,1], num: 98)
+
+func dpCoins(denominations: [Int], num: Int) -> Int {
+  let columns = num + 1
+  let rows = denominations.count + 1
+  
+  var dpTable = [[Int]](repeating: [Int](repeating: 0, count: columns), count: rows)
+  
+  var denominations = denominations.sorted()
+  if denominations[0] != 0 {
+    denominations.insert(0, at: 0)
+  }
+  
+  dpTable[0][0] = 1
+  
+  for i in 1..<rows {
+    dpTable[i][0] = 1
+  }
+  
+  for i in 1..<rows {
+    for j in 1..<columns {
+      if (j - denominations[i]) >= 0 {
+        dpTable[i][j] = dpTable[i - 1][j] + dpTable[i][j - denominations[i]]
+      } else {
+        dpTable[i][j] = dpTable[i - 1][j]
+      }
+    }
+  }
+  return dpTable[rows - 1][columns - 1]
+}
+
+dpCoins(denominations: [25,10,5,1], num: 98)
